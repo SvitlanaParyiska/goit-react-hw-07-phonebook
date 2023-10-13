d68150fb4c915263f6a0c0060197dda874387433
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2023-10-13 18:22:29 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import { Container } from './App.styled';
+import Notification from './Notification/Notification';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectContacts, selectIsLoading, selectError } from 'redux/selectors';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+
 export const App = () => {
+  const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      {contacts.length > 0 ? (
+        <Filter />
+      ) : (
+        <Notification text={'Your phonebook is empty. Add first contact!'} />
+      )}
+      {isLoading && !error && <b>Request in progress...</b>}
+      <ContactList />
+    </Container>
   );
 };
